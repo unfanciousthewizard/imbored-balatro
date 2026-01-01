53b0ca20e49e40c8ffac3ee446b4c16a26096626
unfancy <datdumbkiddo@gmail.com>
2026-01-01 11:45:14 -0800

    new joker work

---
+++ jokers/scentedjoker.lua
@@ -0,0 +1,65 @@
+
+SMODS.Joker{ --Scented Joker
+    key = "scentedjoker",
+    config = {
+        extra = {
+            jokercount = 0,
+            xmult0 = 3
+        }
+    },
+    loc_txt = {
+        ['name'] = 'Scented Joker',
+        ['text'] = {
+            [1] = 'A revolution in joker technol.....',
+            [2] = 'Who toke a bite?!',
+            [3] = '{C:red} #1# {} Mult, scales with jokers owned',
+            [4] = 'If the card resembling Jimbo is owned,',
+            [5] = '{X:mult,C:white}x3{} Mult'
+        },
+        ['unlock'] = {
+            [1] = 'Unlocked by default.'
+        }
+    },
+    pos = {
+        x = 2,
+        y = 1
+    },
+    display_size = {
+        w = 71 * 1, 
+        h = 95 * 1
+    },
+    cost = 4,
+    rarity = 1,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'CustomJokers',
+    pools = { ["imbored_imbored_jokers"] = true },
+    
+    loc_vars = function(self, info_queue, card)
+        
+        return {vars = {(#(G.jokers and (G.jokers and G.jokers.cards or {}) or {})) * 3}}
+    end,
+    
+    calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.joker_main  then
+            if (function()
+                for i, v in pairs(G.jokers.cards) do
+                    if v.config.center.key == "j_joker" then 
+                        return true
+                    end
+                end
+            end)() then
+                return {
+                    Xmult = 3
+                }
+            else
+                return {
+                    mult = (#(G.jokers and G.jokers.cards or {})) * 3
+                }
+            end
+        end
+    end
+}
--- main.lua
+++ main.lua
@@ -60,7 +60,7 @@ local NFS = require("nativefs")
 to_big = to_big or function(a) return a end
 lenient_bignum = lenient_bignum or function(a) return a end
 
-local jokerIndexList = {9,11,4,5,12,2,7,6,3,10,1,8}
+local jokerIndexList = {9,12,4,5,13,2,7,6,3,11,1,8,10}
 
 local function load_jokers_folder()
     local mod_path = SMODS.current_mod.path
@@ -189,6 +189,7 @@ SMODS.ObjectType({
         ["j_imbored_holycrackers"] = true,
         ["j_imbored_parttheseas"] = true,
         ["j_imbored_pulsesog"] = true,
+        ["j_imbored_scentedjoker"] = true,
         ["j_imbored_soniccdspritefromtheprototype"] = true,
         ["j_imbored_sowee"] = true,
         ["j_imbored_you_know"] = true
