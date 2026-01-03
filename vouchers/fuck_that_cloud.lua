af976fc91a2cfc1a094bd4a9c019f7a189374eaf
unfancy <datdumbkiddo@gmail.com>
2026-01-02 16:35:54 -0800

    crisp

--- vouchers/fuck_that_cloud.lua
+++ vouchers/fuck_that_cloud.lua
@@ -4,7 +4,6 @@ SMODS.Voucher {
     pos = { x = 0, y = 0 },
     config = { 
         extra = {
-            all_blinds_size0 = 2,
             winner_ante_value0 = 16
         } 
     },
@@ -25,51 +24,18 @@ SMODS.Voucher {
     can_repeat_soul = false,
     atlas = 'CustomVouchers',
     redeem = function(self, card)
-        G.E_MANAGER:add_event(Event({
-            func = function()
-                G.GAME.starting_params.ante_scaling = G.GAME.starting_params.ante_scaling / 2
-                return true
-            end
-        }))
         return {
-            
-            G.E_MANAGER:add_event(Event({
-                func = function()
-                    G.GAME.round_resets.reroll_cost = G.GAME.round_resets.reroll_cost + 15
-                    G.GAME.current_round.reroll_cost = math.max(0,
-                    G.GAME.current_round.reroll_cost + 15)
-                    return true
-                end
-            }))
-            ,
-            extra = {
+            func = function()
                 
                 G.E_MANAGER:add_event(Event({
                     func = function()
-                        G.GAME.discount_percent = (G.GAME.discount_percent or 0)
-                        for v in pairs(G.I.CARD) do
-                        if v.set_cost then v:set_cost() end
-                            return true
-                        end
+                        G.GAME.win_ante = 16
+                        return true
                     end
                 }))
-                ,
-                colour = G.C.BLUE,
-                extra = {
-                    func = function()
-                        
-                        G.E_MANAGER:add_event(Event({
-                            func = function()
-                                G.GAME.win_ante = 16
-                                return true
-                            end
-                        }))
-                        return true
-                    end,
-                    message = "Winner Ante set to " .. 16 .. "!",
-                    colour = G.C.FILTER
-                }
-            }
+                return true
+            end,
+            message = "Winner Ante set to " .. 16 .. "!"
         }
     end
 }
