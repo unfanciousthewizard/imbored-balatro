eab9582aa3f2ecb29fb53174d83b2335d59b08f5
unfancy <datdumbkiddo@gmail.com>
2026-01-03 20:09:10 -0800

    joker update

--- jokers/thelonelyjoker.lua
+++ jokers/thelonelyjoker.lua
@@ -3,6 +3,7 @@ SMODS.Joker{ --The Lonely Joker
     key = "thelonelyjoker",
     config = {
         extra = {
+            jokercount = 0,
             chips0 = 20,
             mult0 = 10
         }
@@ -13,7 +14,9 @@ SMODS.Joker{ --The Lonely Joker
             [1] = 'When {C:red}alone{},',
             [2] = '{C:blue}+20{} Chips and {C:red}+10{} Mult',
             [3] = 'When {C:green}not alone{},',
-            [4] = '{C:green}Copy{} joker to the right.'
+            [4] = '{C:green}Multiply{} those by',
+            [5] = 'The number of',
+            [6] = '{C:attention}Jokers{} in hand.'
         },
         ['unlock'] = {
             [1] = 'Unlocked by default.'
@@ -37,6 +40,11 @@ SMODS.Joker{ --The Lonely Joker
     atlas = 'CustomJokers',
     pools = { ["imbored_imbored_jokers"] = true },
     
+    loc_vars = function(self, info_queue, card)
+        
+        return {vars = {(#(G.jokers and (G.jokers and G.jokers.cards or {}) or {})) * 20}}
+    end,
+    
     calculate = function(self, card, context)
         if context.cardarea == G.jokers and context.joker_main  then
             if to_big(#G.jokers.cards) == to_big(1) then
@@ -44,6 +52,13 @@ SMODS.Joker{ --The Lonely Joker
                     chips = 20,
                     extra = {
                         mult = 10
+                    }
+                }
+            elseif to_big(#G.jokers.cards) > to_big(1) then
+                return {
+                    chips = (#(G.jokers and G.jokers.cards or {})) * 20,
+                    extra = {
+                        mult = (#(G.jokers and G.jokers.cards or {})) * 20
                     }
                 }
             end
