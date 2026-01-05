0c325cc04b887a50c21178201e38e4b36e39e76c
unfancy <datdumbkiddo@gmail.com>
2026-01-04 20:11:08 -0800

    evil changed

--- editions/evil.lua
+++ editions/evil.lua
@@ -1,9 +1,9 @@
 
-SMODS.Shader({ key = 'greyscale', path = 'greyscale.fs' })
+SMODS.Shader({ key = 'agressivelyred', path = 'agressivelyred.fs' })
 
 SMODS.Edition {
     key = 'evil',
-    shader = 'greyscale',
+    shader = 'agressivelyred',
     in_shop = false,
     weight = 3.5,
     extra_cost = 5,
@@ -25,28 +25,13 @@ SMODS.Edition {
     get_weight = function(self)
         return G.GAME.edition_rate * self.weight
     end,
-    
     calculate = function(self, card, context)
         if context.pre_joker or (context.main_scoring and context.cardarea == G.play) then
-            assert(SMODS.change_base(card, pseudorandom_element(SMODS.Suits, 'edit_card_suit').key, pseudorandom_element(SMODS.Ranks, 'edit_card_rank').key))
             return {
-                message = "yah.....im evil"
-            }
-        end
-        if context.cardarea == G.hand and context.main_scoring then
-            return {
-                func = function()
-                    assert(SMODS.change_base(card, pseudorandom_element(SMODS.Suits, 'edit_card_suit').key, pseudorandom_element(SMODS.Ranks, 'edit_card_rank').key))
-                end,
-                message = "yah.....im evil"
-            }
-        end
-        if context.discard and context.other_card == card then
-            return {
-                func = function()
-                    assert(SMODS.change_base(context.other_card, pseudorandom_element(SMODS.Suits, 'edit_card_suit').key, pseudorandom_element(SMODS.Ranks, 'edit_card_rank').key))
-                end,
-                message = "yah.....im evil"
+                chips = -20,
+                extra = {
+                    mult = 2
+                }
             }
         end
     end
