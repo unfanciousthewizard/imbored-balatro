a856ad9823e30c3b993ff2a3e77dd4427b003d32
unfancy <datdumbkiddo@gmail.com>
2026-01-21 18:31:42 -0800

    rework

--- jokers/stencilprint.lua
+++ jokers/stencilprint.lua
@@ -3,13 +3,17 @@ SMODS.Joker{ --Stencilprint
     key = "stencilprint",
     config = {
         extra = {
-            JokerCountSHIT = 1
+            JokerCountSHIT = 1,
+            odds = 4
         }
     },
     loc_txt = {
         ['name'] = 'Stencilprint',
         ['text'] = {
-            [1] = '{C:green}3/6{} chance to {C:green}copy{} {C:attention}joker{} to the right'
+            [1] = '{C:green}1/4{} Chance to',
+            [2] = '{C:green}Clone{} {C:attention}Joker{} to the Right,',
+            [3] = 'When {C:purple}Blind{} is Selected,',
+            [4] = '{C:red}Destroys{} Self Afterward.'
         },
         ['unlock'] = {
             [1] = 'Unlocked by default.'
@@ -23,8 +27,8 @@ SMODS.Joker{ --Stencilprint
         w = 71 * 1, 
         h = 95 * 1
     },
-    cost = 5,
-    rarity = 2,
+    cost = 6,
+    rarity = 3,
     blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
@@ -35,6 +39,57 @@ SMODS.Joker{ --Stencilprint
     
     loc_vars = function(self, info_queue, card)
         
-        return {vars = {card.ability.extra.JokerCountSHIT}}
+        local new_numerator, new_denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'j_imbored_stencilprint') 
+        return {vars = {card.ability.extra.JokerCountSHIT, new_numerator, new_denominator}}
+    end,
+    
+    calculate = function(self, card, context)
+        if context.setting_blind  then
+            if true then
+                if SMODS.pseudorandom_probability(card, 'group_0_8389747b', 1, card.ability.extra.odds, 'j_imbored_stencilprint', false) then
+                    SMODS.calculate_effect({func = function()
+                        local my_pos = nil
+                        for i = 1, #G.jokers.cards do
+                            if G.jokers.cards[i] == card then
+                                my_pos = i
+                                break
+                            end
+                        end
+                        local target_joker = (my_pos and my_pos < #G.jokers.cards) and G.jokers.cards[my_pos + 1] or nil
+                        
+                        if target_joker then
+                            G.GAME.joker_buffer = G.GAME.joker_buffer + 1
+                            G.E_MANAGER:add_event(Event({
+                                func = function()
+                                    local copied_joker = copy_card(target_joker, nil, nil, nil, target_joker.edition and target_joker.edition.negative)
+                                    
+                                    copied_joker:add_to_deck()
+                                    G.jokers:emplace(copied_joker)
+                                    G.GAME.joker_buffer = 0
+                                    return true
+                                end
+                            }))
+                            card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = localize('k_duplicated_ex'), colour = G.C.GREEN})
+                        end
+                        return true
+                    end}, card)
+                    SMODS.calculate_effect({func = function()
+                        local target_joker = card
+                        
+                        if target_joker then
+                            target_joker.getting_sliced = true
+                            G.E_MANAGER:add_event(Event({
+                                func = function()
+                                    target_joker:start_dissolve({G.C.RED}, nil, 1.6)
+                                    return true
+                                end
+                            }))
+                            card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = "Destroyed!", colour = G.C.RED})
+                        end
+                        return true
+                    end}, card)
+                end
+            end
+        end
     end
 }
--- jokers/transparint.lua
+++ jokers/transparint.lua
@@ -8,7 +8,10 @@ SMODS.Joker{ --Transparint
     loc_txt = {
         ['name'] = 'Transparint',
         ['text'] = {
-            [1] = '{C:green}0/10{} chance to {C:green}copy{} {C:attention}joker{} to the right'
+            [1] = '{C:green}Copies{} {C:attention}joker{} to the right,',
+            [2] = 'when blind is {C:red}defeated{},',
+            [3] = '{C:blue}replaces{} self with',
+            [4] = '{C:attention}joker{} to the right.'
         },
         ['unlock'] = {
             [1] = 'Unlocked by default.'
@@ -22,13 +25,80 @@ SMODS.Joker{ --Transparint
         w = 71 * 1, 
         h = 95 * 1
     },
-    cost = 4,
-    rarity = 1,
+    cost = 6,
+    rarity = 3,
     blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
     unlocked = true,
     discovered = true,
     atlas = 'CustomJokers',
-    pools = { ["imbored_imbored_jokers"] = true }
+    pools = { ["imbored_imbored_jokers"] = true },
+    
+    calculate = function(self, card, context)
+        if context.starting_shop  then
+            return {
+                func = function()
+                    local my_pos = nil
+                    for i = 1, #G.jokers.cards do
+                        if G.jokers.cards[i] == card then
+                            my_pos = i
+                            break
+                        end
+                    end
+                    local target_joker = (my_pos and my_pos < #G.jokers.cards) and G.jokers.cards[my_pos + 1] or nil
+                    
+                    if target_joker then
+                        G.GAME.joker_buffer = G.GAME.joker_buffer + 1
+                        G.E_MANAGER:add_event(Event({
+                            func = function()
+                                local copied_joker = copy_card(target_joker, nil, nil, nil, target_joker.edition and target_joker.edition.negative)
+                                
+                                copied_joker:add_to_deck()
+                                G.jokers:emplace(copied_joker)
+                                G.GAME.joker_buffer = 0
+                                return true
+                            end
+                        }))
+                        card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = localize('k_duplicated_ex'), colour = G.C.GREEN})
+                    end
+                    return true
+                end,
+                extra = {
+                    func = function()
+                        local target_joker = card
+                        
+                        if target_joker then
+                            target_joker.getting_sliced = true
+                            G.E_MANAGER:add_event(Event({
+                                func = function()
+                                    target_joker:start_dissolve({G.C.RED}, nil, 1.6)
+                                    return true
+                                end
+                            }))
+                            card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = "Destroyed!", colour = G.C.RED})
+                        end
+                        return true
+                    end,
+                    colour = G.C.RED
+                }
+            }
+        end
+        
+        local target_joker = nil
+        
+        local my_pos = nil
+        for i = 1, #G.jokers.cards do
+            if G.jokers.cards[i] == card then
+                my_pos = i
+                break
+            end
+        end
+        target_joker = (my_pos and my_pos < #G.jokers.cards) and G.jokers.cards[my_pos + 1] or nil
+        
+        local ret = SMODS.blueprint_effect(card, target_joker, context)
+        if ret then
+            SMODS.calculate_effect(ret, card)
+        end
+    end
 }
